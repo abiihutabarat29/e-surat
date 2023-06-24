96aa4a0a5d1481a0559b2f4b574b30a0cdc4a5a5
abiihutabarat29 <abiihutabarat97@gmail.com>
2023-06-24 12:21:28 +0700

    update footer

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,7 +1,8 @@
 <!-- /.content-wrapper -->
 <footer class="main-footer">
-    <strong>Copyright &copy; e-surat</strong>
-    All rights reserved.
+    <strong>Copyright &copy;<?= date('Y') ?> <a href="https://diskominfo.batubarakab.go.id" target="blank">Dinas
+            Komunikasi dan
+            Informatika Kab. Batu Bara</a> All rights reserved.</strong>
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 1.0
     </div>
